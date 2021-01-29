08a357e5c0a9126b18db249cc6ea49f9f943a0ef
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-01-29 09:24:15 -0500

    Finished the trident problem and added comments to it

--- Trident/main.swift
+++ Trident/main.swift
@@ -25,6 +25,13 @@ print("Enter tine spacing:")
 print("   ", terminator: "")
 let s = Int(readLine()!)!
 
+//Get handle length
+print("Enter handle length")
+print("   ", terminator: "")
+let h = Int(readLine()!)!
+let l = s + 1
+
+
 // OUTPUT
 
 //Draw all tines
@@ -34,7 +41,7 @@ for _ in 1...t {
     for _ in 1...3 {
         //Print star
         print("*", terminator: "")
-
+        
         for _ in 1...s {
             //Print the spaces
             print(" ", terminator: "")
@@ -48,6 +55,29 @@ for _ in 1...t {
     
 }
 
+//Draw the horizontal part of the trident
+//2 * s covers the spaces in between the tines
+//+ 3 fills the empty space just under the tines
+//(The code doesn't print it exactly like that, but it's just the idea behind how I came up with the numbers)
+let horizontal = 2 * s + 3
+print("", terminator: "")
+for _ in 1...horizontal {
+    print("*", terminator: "")
+}
+//Goes to next line to start printing the handle
+print("", terminator: "\n")
+
+
+//Draw handle
+for _ in 1...h {
+    
+    //Prints spaces so the handle is at the center of the trident
+    for _ in 1...l {
+        print(" ", terminator: "")
+    }
+    //Prints stars in different lines
+    print("*", terminator: "\n")
+}
 
 
 
